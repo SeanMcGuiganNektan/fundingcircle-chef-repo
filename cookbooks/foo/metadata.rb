c471c619cb77736d0f2e0804abe5efc27ccae62c
Sean McGuigan <sean.mcguigan@fundingcircle.com>
2016-03-15 14:25:40 +0000

    Inital Commit

---
+++ cookbooks/foo/metadata.rb
@@ -0,0 +1,7 @@
+name             'foo'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures foo'
+long_description 'Installs/Configures foo'
+version          '0.1.0'
